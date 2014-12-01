cd4df69c51b1ae604057263be689d7e6792ed250
jgrelet <jacques.grelet@ird.fr>
2014-12-01 20:10:58 +1100

    add startup

---
+++ m/initial_dir/examples/pirata-fr24/startup.m
@@ -0,0 +1,16 @@
+% LADCP processing 10.0
+
+% addpath ../m/ladcp_1.1;
+% addpath ../m/sw;
+% addpath m;
+
+addpath c:/git/ladcp/m/ladcp;
+addpath c:/git/ladcp/m/sw;
+addpath c:/git/ladcp/m;
+addpath m;
+
+
+set(0,'defaultsurfaceedgecolor','none');
+%set(0,'defaultfigurerenderer','zbuffer')
+%set(0,'defaultfigurerenderer','opengl')
+set(0,'defaultfigurerenderer','painter')
