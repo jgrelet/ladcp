775d7b6cb7f96a3ba71163a89acce7f760764b0e
jgrelet <jacques.grelet@ird.fr>
2014-12-01 15:59:45 +1100

    move down if p.edit_mask_last_bin(1)~=0 remove plot_controls.m

--- m/ladcp/edit_data.m
+++ m/ladcp/edit_data.m
@@ -12,7 +12,7 @@ function d = edit_data(d,p,values)
 % moving maxbinrange to here                      GK, 24.07.2008  0.3-->0.4
 % more than last bin masking                      GK, 13.05.2011  0.4-->0.5
 % renamed cosd and sind to cos_d and sin_d        GK, 13.07.2012  0.5-->0.6
-% use sfigure instead of figure                   GK, 16.11.2012  0.6-->0.7   
+% use sfigure instead of figure                   GK, 16.11.2012  0.6-->0.7
 
 %
 % general function info
@@ -50,13 +50,13 @@ end
 % Bin Masking
 %----------------------------------------------------------------------
 if ~isempty(p.edit_mask_dn_bins) | ~isempty(p.edit_mask_up_bins)
-
+  
   nbad = 0;
   if length(d.zu) > 0
     for bi=1:length(p.edit_mask_up_bins)
       bn = length(d.zu)+1 - p.edit_mask_up_bins(bi);
       nbad = nbad + length(find(isfinite(d.weight(bn,:))));
-      d.weight(bn,:) = NaN; 
+      d.weight(bn,:) = NaN;
       d.ts_edited(bn,:) = NaN;
     end
   end
@@ -64,13 +64,13 @@ if ~isempty(p.edit_mask_dn_bins) | ~isempty(p.edit_mask_up_bins)
     for bi=1:length(p.edit_mask_dn_bins)
       bn = length(d.zu) + p.edit_mask_dn_bins(bi);
       nbad = nbad + length(find(isfinite(d.weight(bn,:))));
-      d.weight(bn,:) = NaN; 
+      d.weight(bn,:) = NaN;
       d.ts_edited(bn,:) = NaN;
     end
   end
-
+  
   disp(sprintf('    Bin masking               : set %d weights to NaN',nbad));
-
+  
 end % if bin masking enabled
 
 %----------------------------------------------------------------------
@@ -78,7 +78,7 @@ end % if bin masking enabled
 %----------------------------------------------------------------------
 
 if p.edit_sidelobes
-
+  
   nbad = 0;
   
   % first, the uplooker: d.z is -ve distance of ADCP from surface;
@@ -86,39 +86,39 @@ if p.edit_sidelobes
   % in m --- the same value used by Firing's software
   
   if length(d.zu > 0)
-  
-%    for b=1:length(d.zu)+length(d.zd)
-% this loops over all bins (up and down), changing that
-% to only up, GK Sep 2007
+    
+    %    for b=1:length(d.zu)+length(d.zd)
+    % this loops over all bins (up and down), changing that
+    % to only up, GK Sep 2007
     zlim = d.izm;
     for b=d.izu
       zlim(b,:) = (1 - cos_d(d.up.Beam_angle)) * d.z ...
-		- 0.015*d.up.Cell_length;
+        - 0.015*d.up.Cell_length;
     end
     ibad = find(d.izm > zlim);
     nbad = nbad + length(find(isfinite(d.weight(ibad))));
-    d.weight(ibad) = NaN; 
+    d.weight(ibad) = NaN;
     d.ts_edited(ibad) = NaN;
-  
+    
   end
-
+  
   % now, the downlooker: p.zbottom is the +ve depth of the sea bed; therefore,
   % -d.z - p.zbottom is the -ve distance from the sea bed
- 
-% same as above, changing to only down, GK Sep 2007
+  
+  % same as above, changing to only down, GK Sep 2007
   zlim = d.izm;
   for b = d.izd
     zlim(b,:) = -p.zbottom ...
-	      + (1 - cos_d(d.down.Beam_angle)) * (d.z+p.zbottom) ...
-	      + 0.015*d.down.Cell_length;
+      + (1 - cos_d(d.down.Beam_angle)) * (d.z+p.zbottom) ...
+      + 0.015*d.down.Cell_length;
   end
   ibad = find(d.izm < zlim);
   nbad = nbad + length(find(isfinite(d.weight(ibad))));
-  d.weight(ibad) = NaN; 
+  d.weight(ibad) = NaN;
   d.ts_edited(ibad) = NaN;
   
   disp(sprintf('    Side-lobe contamination   : set %d weights to NaN',nbad));
-
+  
 end %if p.edit_sidelobes
 
 %----------------------------------------------------------------------
@@ -126,17 +126,17 @@ end %if p.edit_sidelobes
 %----------------------------------------------------------------------
 
 if ~isnan(p.edit_spike_filter_max_curv)
-
+  
   nbad = 0;
   for b=[1:size(d.ts,1)]
     dummy = d.ts(b,:)/rms(d.ts(b,:));
     ibad = find(diff(diff(dummy)) < -1*p.edit_spike_filter_max_curv) + 1;
     nbad = nbad + length(find(isfinite(d.weight(b,ibad))));
-    d.weight(b,ibad) = NaN; 
+    d.weight(b,ibad) = NaN;
     d.ts_edited(b,ibad) = NaN;
   end
   disp(sprintf('    Spike filter              : set %d weights to NaN',nbad));
-
+  
 end %if p.edit_spike_filter
 
 
@@ -145,7 +145,7 @@ end %if p.edit_spike_filter
 %----------------------------------------------------------------------
 
 if p.edit_PPI
-
+  
   % NB: at present, PPI filtering is only implemented for the downlooker
   
   nbad = 0;
@@ -162,8 +162,8 @@ if p.edit_PPI
   if isfield(d,'ctdprof_z') & isfield(d,'ctdprof_ss')
     guess_z = -p.zbottom + 1500 * nmean(dt) / 2;
     SS = nmean(d.ctdprof_ss(find(d.ctdprof_z > -guess_z)));
-    if ~isfinite(SS), 
-      SS = 1500; 
+    if ~isfinite(SS),
+      SS = 1500;
     end
   else
     SS = 1500;
@@ -189,11 +189,11 @@ if p.edit_PPI
   for b=d.izd
     ibad = find(d.izm(b,2:end) > PPI_min_z & d.izm(b,2:end) < PPI_max_z) + 1;
     nbad = nbad + length(find(isfinite(d.weight(b,ibad))));
-    d.weight(b,ibad) = NaN; 
+    d.weight(b,ibad) = NaN;
     d.ts_edited(b,ibad) = NaN;
   end
   disp(sprintf('    Previous-ping interference: set %d weights to NaN',nbad));
-
+  
 end %if p.edit_PPI
 
 %----------------------------------------------------------------------
@@ -201,7 +201,7 @@ end %if p.edit_PPI
 %----------------------------------------------------------------------
 
 if ~isempty(p.edit_skip_ensembles)
-
+  
   nskipped = 0;
   iskip = [];
   for i=1:length(p.edit_skip_ensembles)
@@ -209,15 +209,15 @@ if ~isempty(p.edit_skip_ensembles)
       iskip = [iskip i:length(p.edit_skip_ensembles):length(d.time_jul)];
     end
   end
-
+  
   for b=1:length(d.zd)+length(d.zu)
     nskipped = nskipped + length(find(isfinite(d.weight(b,iskip))));
-    d.weight(b,iskip) = NaN; 
+    d.weight(b,iskip) = NaN;
     d.ts_edited(b,iskip) = NaN;
   end
-
+  
   disp(sprintf('    Ensemble skipping         : set %d weights to NaN',nskipped));
-
+  
 end % if p.edit_skip_ensembles enabled
 
 
@@ -225,29 +225,30 @@ end % if p.edit_skip_ensembles enabled
 % Last Bin Masking
 %----------------------------------------------------------------------
 
-if p.edit_mask_last_bin(1)~=0
+if length(p.edit_mask_last_bin)>1
+  mask_dn = p.edit_mask_last_bin(1);
+  mask_up = p.edit_mask_last_bin(2);
+else
+  mask_dn = p.edit_mask_last_bin;
+  mask_up = p.edit_mask_last_bin;
+end
 
-  if length(p.edit_mask_last_bin)>1
-    mask_dn = p.edit_mask_last_bin(1);
-    mask_up = p.edit_mask_last_bin(2);
-  else
-    mask_dn = p.edit_mask_last_bin;
-    mask_up = p.edit_mask_last_bin;
-  end
- 
+nbad = 0;
+dummy = d.weight;
   
-  nbad = 0;
-  dummy = d.weight;
+if p.edit_mask_last_bin(1)~=0
+  
+
   for n=1:size(d.weight,2)
-%    ind = find(~isnan(d.weight(:,n)));
+    %    ind = find(~isnan(d.weight(:,n)));
     ind = find(d.weight(:,n)>0);
-    if ~isempty(ind) 
-%      if ind(1)~=1
-%        d.weight(ind(1),n) = nan;
-%      end
-%      if ind(end)~=size(d.weight,1)
-%        d.weight(ind(end),n) = nan;
-%      end
+    if ~isempty(ind)
+      %      if ind(1)~=1
+      %        d.weight(ind(1),n) = nan;
+      %      end
+      %      if ind(end)~=size(d.weight,1)
+      %        d.weight(ind(end),n) = nan;
+      %      end
       for m=1:mask_up
         if m<=length(ind)
           d.weight(ind(m),n) = nan;
@@ -288,9 +289,9 @@ else
     d.weight(d.izd,:) = d.weight(d.izd,:).*mask;
   end
   nbad = length(find(isnan(dummy)-isnan(d.weight)));
-
+  
   disp(sprintf('    Last Bin masking           : set %d weights to NaN',nbad));
-
+  
 end % if bin masking enabled
 
 
@@ -369,11 +370,11 @@ end
 %----------------------------------------------------------------------
 
 bin_no = [0];
-if length(d.zu) > 0 
-  bin_no = [-length(d.zu):1 bin_no]; 
+if length(d.zu) > 0
+  bin_no = [-length(d.zu):1 bin_no];
 end
-if length(d.zd) > 0 
-  bin_no = [bin_no 1:length(d.zd)]; 
+if length(d.zd) > 0
+  bin_no = [bin_no 1:length(d.zd)];
 end
 
 sfigure(2);
@@ -383,10 +384,10 @@ colormap([[1 1 1]; jet(128)]);
 
 subplot(2,1,1);
 imagesc([1:size(d.ts,2)],bin_no,...
-	[d.ts(1:length(d.zu),:); ...
-	 ones(1,size(d.ts,2))*NaN; ...
-	 d.ts(size(d.ts,1)-length(d.zd)+1:end,:)...
-        ]);
+  [d.ts(1:length(d.zu),:); ...
+  ones(1,size(d.ts,2))*NaN; ...
+  d.ts(size(d.ts,1)-length(d.zd)+1:end,:)...
+  ]);
 csc = caxis;
 colorbar
 xlabel('Ensemble #');
@@ -395,10 +396,10 @@ title('Before Data Editing');
 
 subplot(2,1,2);
 imagesc([1:size(d.ts,2)],bin_no,...
-	[d.ts_edited(1:length(d.zu),:); ...
-	 ones(1,size(d.ts,2))*NaN; ...
-	 d.ts_edited(size(d.ts,1)-length(d.zd)+1:end,:)...
-        ]);
+  [d.ts_edited(1:length(d.zu),:); ...
+  ones(1,size(d.ts,2))*NaN; ...
+  d.ts_edited(size(d.ts,1)-length(d.zd)+1:end,:)...
+  ]);
 csc = caxis;
 colorbar
 xlabel('Ensemble #');
@@ -419,10 +420,10 @@ colormap([[1 1 1]; jet(128)]);
 
 subplot(2,1,1);
 imagesc([1:size(d.cm,2)],bin_no,...
-	[d.cm(1:length(d.zu),:); ...
-	 ones(1,size(d.cm,2))*NaN; ...
-	 d.cm(size(d.cm,1)-length(d.zd)+1:end,:)...
-        ]);
+  [d.cm(1:length(d.zu),:); ...
+  ones(1,size(d.cm,2))*NaN; ...
+  d.cm(size(d.cm,1)-length(d.zd)+1:end,:)...
+  ]);
 csc = caxis;
 colorbar
 xlabel('Ensemble #');
@@ -431,10 +432,10 @@ title('Before Data Editing');
 
 subplot(2,1,2);
 imagesc([1:size(d.cm,2)],bin_no,...
-	[d.cm_edited(1:length(d.zu),:); ...
-	 ones(1,size(d.cm,2))*NaN; ...
-	 d.cm_edited(size(d.cm,1)-length(d.zd)+1:end,:)...
-        ]);
+  [d.cm_edited(1:length(d.zu),:); ...
+  ones(1,size(d.cm,2))*NaN; ...
+  d.cm_edited(size(d.cm,1)-length(d.zd)+1:end,:)...
+  ]);
 csc = caxis;
 colorbar
 xlabel('Ensemble #');
